1418f90c41108fd95dffe2d32deaa302b9815323
Sabahattin Giritli <sabahattin2005@hotmail.de>
2020-02-23 19:39:16 +0100

    Bugfix: fixed production base path.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 process.env.VUE_APP_VERSION = require("./package.json").version;
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/workout-companion/'
+    : '/'
 };
